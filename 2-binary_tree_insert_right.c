9360056ea11ed371fba31cc3581b1198973c3d82
Abir <125511720+Cloudyheaveen@users.noreply.github.com>
2023-08-30 21:19:41 +0000

    ARAHOU ABIR

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,11 +5,7 @@
  * the right-child of another node
  * @parent: A pointer to the node to insert the right-child in
  * @value: The value to store in the new node
-<<<<<<< HEAD
- *
-=======
  * 
->>>>>>> 4608ace0b04afbbd0a431b43eadf99ba194a6def
  * Return: A pointer to the created node,
  * or NULL on failure or if parent is NULL
  *
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,11 +3,7 @@
 /**
  * binary_tree_is_leaf - Function that checks if a node is a leaf
  * @node: A pointer to the node to check
-<<<<<<< HEAD
- *
-=======
  * 
->>>>>>> 4608ace0b04afbbd0a431b43eadf99ba194a6def
  * Return: 1 if node is a leaf, otherwise 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
